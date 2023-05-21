3a64a9615f4d7e271b1875eb6b247a729d2cb927
LiiNen <kjeonghoon065@gmail.com>
2023-05-21 16:12:02 +0900

    Add checklist default

--- lib/checklist_view.dart
+++ lib/checklist_view.dart
@@ -6,12 +6,32 @@ class ChecklistView extends StatefulWidget {
 }
 
 class _ChecklistView extends State<ChecklistView> {
+  List<String> todoList = [
+    '코딩테스트 공부',
+    'A과목 복습',
+    'B과목 과제',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('checklist')),
       body: Container(
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Text(todoList[0]),
+            ),
+            Container(
+              child: Text(todoList[1]),
+            ),
+            Container(
+              child: Text(todoList[2])
+            )
+          ]
+        )
       )
     );
   }
