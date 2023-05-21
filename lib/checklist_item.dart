4e92b74274a1107543684bfa31a972d314fc55ac
LiiNen <kjeonghoon065@gmail.com>
2023-05-21 16:24:46 +0900

    Add check action

--- lib/checklist_item.dart
+++ lib/checklist_item.dart
@@ -9,12 +9,26 @@ class ChecklistItem extends StatefulWidget {
 }
 
 class _ChecklistItem extends State<ChecklistItem> {
+  bool _isChecked = false;
+
+  void _check() {
+    setState(() {
+      _isChecked = !_isChecked;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: _check,
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Text(widget.text)
+        child: Text(widget.text,
+          style: TextStyle(
+            color: _isChecked ? Colors.grey : Colors.black,
+            decoration: _isChecked ? TextDecoration.lineThrough : null
+          )
+        )
       )
     );
   }
